2390ecf7ac239e5263382a52e8fe5cd57534f7d6
DenisMl <denismldn@gmail.com>
2018-12-14 15:48:56 +0300

    small fixes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,5 @@
 let express = require('express');
 let router = express.Router();
-let huila = require('../public/javascripts/stub');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
